635315afb658be758f67dce2e2c411bcb079294e
Francisco José Fajardo Toril <frannnlol@gmail.com>
2015-07-09 14:33:54 +0200

    Added Toolbar as new Action Bar.

--- app/src/main/java/com/example/fran/workout/MainActivity.java
+++ app/src/main/java/com/example/fran/workout/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.fran.workout;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declare toolbar object by id.
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
